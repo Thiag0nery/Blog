65c60a9192e39421b7828fe98d7496e87be8b5a0
thiag0nery <thiagonery456@gmail.com>
2023-03-27 22:27:00 -0300

    inicio

---
+++ posts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import UpdateView
+from django.http import HttpResponse
+
+class PostIndex(ListView):
+    ...
+class PostBusca(ListView):
+    ...
+class PostCategoria(ListView):
+    ...
+class PostDetalhes(UpdateView):
+    ...
